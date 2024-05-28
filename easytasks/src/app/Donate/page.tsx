f61d456a8a642493220006d86bed86f68e41bd68
yesenia <aryesigon@gmail.com>
2024-05-28 19:51:54 -0300

    se realizo la navbar con el boton iniciar sesion

---
+++ easytasks/src/app/Donate/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Donate = () => {
+  return (
+    <div>
+      <h1>Donaciones</h1>
+      <p>Proyecto final Henry-Gestor de tareas</p>
+    </div>
+  );
+};
+
+export default Donate;
