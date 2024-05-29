57bf21f7b4d026ee1734756c5ba9782147dff393
Samanta Driuzzi <driuzzi.samanta.mayra@gmail.com>
2024-05-29 00:41:46 -0300

    Finalizada la lógica para la visualización de cada beneficio, falta completar con los diseños y queda la sección completa

---
+++ easytasks/src/app/page.tsx
@@ -0,0 +1,10 @@
+// "use client";
+// import { useRouter } from "next/navigation";
+
+import BenefitsSection from "@/components/benefits-section/benefits-section";
+
+export default function Home() {
+  // const router = useRouter();
+  // return router.push("/landing");
+  return <BenefitsSection />;
+}
---
+++ easytasks/src/utils/benefits-section/benefits.ts
@@ -0,0 +1,36 @@
+const benefits = [
+  {
+    id: 1,
+    title: "Mantén tus tareas ordenadas por prioridad en un solo lugar",
+    image: "/benefits-section/prioridad.svg",
+    alt: "gatos ordenando tareas en una pantalla",
+  },
+  {
+    id: 2,
+    title: "Forma equipos y alcanza objetivos en grupo.",
+    image: "/benefits-section/gatosEnMeet.svg",
+    alt: "gatos en una reunión virtual",
+  },
+  {
+    id: 3,
+    title:
+      "Activa el modo Bienestar para mantenerte enfocado y evitar sobrecargas",
+    image: "/benefits-section/modobienestar.svg",
+    alt: "gato contento, con manos levantadas",
+  },
+  {
+    id: 4,
+    title:
+      "Por cada objetivo logrado recibe tu recompensa y aumenta tu motivación.",
+    image: "/benefits-section/recompensa.svg",
+    alt: "medallas y trofeos",
+  },
+  {
+    id: 5,
+    title:
+      "Sé parte de esta hermosa comunidad y colabora para las próximas mejoras",
+    image: "/benefits-section/comunidad.svg",
+    alt: "gatos donando dinero",
+  },
+];
+export default benefits;
