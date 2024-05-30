4a684b7b33bd93d69c7772e57d5f41790159f6c7
Sam <driuzzi.samanta.mayra@gmail.com>
2024-05-30 12:39:11 -0300

    Revert "Develop"

---
+++ easytasks/src/app/Landing/page.tsx
@@ -0,0 +1,11 @@
+import Bienvenida from "@/components/Bienvenida";
+
+const Landing: React.FC = () => {
+  return (
+    <div className="bg-gradient-to-b from-blue-dark via-blue-medium to-blue-lighter min-h-screen text-white">
+      <Bienvenida />
+    </div>
+  );
+};
+
+export default Landing;
