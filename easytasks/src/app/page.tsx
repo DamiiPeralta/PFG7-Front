c6a6b24290da826217de8dc07aac0cb85fb1644e
Delfina Merlo <137831158+D-MERLO@users.noreply.github.com>
2024-05-29 23:29:08 -0300

    Se realizan las secciones de la landing de SaludMental y QuienesSomos

--- easytasks/src/app/page.tsx
+++ easytasks/src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import AboutUs from "../components/aboutUs";
+import MentalHealth from "../components/mentalHealth";
 
 export default function Home() {
-  return <h1>Hola</h1>;
+  return (
+    <>
+
+    </>
+  );
 }
--- easytasks/tailwind.config.ts
+++ easytasks/tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "color1": "#329FA6",
+        "color2": "#5CB0C8",
+        "color3": "#AF87EA",
+        "color4": "#6C5DBC",
+        "color5": "#4A48A4",
+        "color6": "#29338D",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
