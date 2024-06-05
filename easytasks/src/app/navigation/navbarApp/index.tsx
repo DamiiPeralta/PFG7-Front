45c2e1495d0d0dbf93e6469963bcb04f8c4973ad
yesenia <aryesigon@gmail.com>
2024-06-04 21:59:31 -0300

    coloque los enlace navegacion del navbar app mis equipos unirse y crear equipos

--- easytasks/src/app/navigation/navbarApp/index.tsx
+++ easytasks/src/app/navigation/navbarApp/index.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const NavbarApp = () => {
-  const handleSelectChange = (event: { target: { value: any; }; }) => {
+  const handleSelectChange = (event: { target: { value: any } }) => {
     const value = event.target.value;
     if (value) {
       window.location.href = value;
@@ -13,13 +13,20 @@ const NavbarApp = () => {
   return (
     <div className=" w-full h-30 bg-color5 fixed top-0 p-4 z-20">
       <div className="flex flex-row justify-between items-center">
-          <Link href="/home" className="flex">
-            <img src="/logo.svg" alt="logo" className="w-26 h-14" />
-            <Image src="/EasyTasks.svg" alt="AppName" width={100} height={100}/>
-          </Link>
+        <Link href="/home" className="flex">
+          <img src="/logo.svg" alt="logo" className="w-26 h-14" />
+          <Image src="/EasyTasks.svg" alt="AppName" width={100} height={100} />
+        </Link>
         <nav className="flex space-x-8">
-          <a href="/home" className="hover:text-gray-200 transition duration-300">Inicio</a>
-          <a href="#" className="hover:text-gray-200 transition duration-300">Tablero</a>
+          <a
+            href="/home"
+            className="hover:text-gray-200 transition duration-300"
+          >
+            Inicio
+          </a>
+          <a href="#" className="hover:text-gray-200 transition duration-300">
+            Tablero
+          </a>
           <select
             onChange={handleSelectChange}
             className="bg-color5 text-black hover:text-gray-200 transition duration-300"
@@ -27,9 +34,9 @@ const NavbarApp = () => {
             <option value="" disabled selected>
               Equipos
             </option>
-            <option value="#">Mis Equipos</option>
-            <option value="#">Unirse</option>
-            <option value="#">Crear</option>
+            <option value="/mis-equipos">Mis Equipos</option>
+            <option value="/unirse">Unirse</option>
+            <option value="/crear-equipo">Crear</option>
             <option value="#">Información</option>
           </select>
         </nav>
